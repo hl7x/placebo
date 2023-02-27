734ee5017d5136b1172b8e5fb09d74e43239f929
Nick Mancari <nickjmancari@gmail.com>
2023-02-26 19:14:46 -0600

    option for multiple random instances

--- pkg/random/patient.go
+++ pkg/random/patient.go
@@ -23,9 +23,10 @@ type Patient struct {
 
 func NewPatients(max int) Collection {
 	var tmp []Patient
-	patientInstance := NewPatient()
+//	patientInstance := NewPatient()
 
 	for i := 0; i < max; i++ {
+		patientInstance := NewPatient()
 		tmp = append(tmp, patientInstance)
 	}
 
