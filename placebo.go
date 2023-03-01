3e8660d73676e27639a0b7bd9ac748e886c548e5
Nick Mancari <nickjmancari@gmail.com>
2023-03-01 16:33:19 -0600

    clean up

--- placebo.go
+++ placebo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-//	"os"
 	
 	"placebo/cmd"
 )
@@ -10,11 +9,8 @@ import (
 var randomPatientFile = flag.String("file", "", "Creates a csv file with a fake patient at /tmp/.\n\t'init' command creates a random file quickly.\n\t\tadding numbers to the 'init' command will produce multiple fake patients i.e. 'placebo --file init 4'")
 
 func main() {
-//	flag := os.Args[2]
-//	cmd := os.Args[3]
 	flag.Parse()
 	
 	cmd.PatientFile(*randomPatientFile)
-//	cmd.PatientFile(flag, cmd)
 
 }
