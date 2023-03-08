aee3d9659d0297d870f32c4397f19302f8f4f5dc
Nick Mancari <nickjmancari@gmail.com>
2023-03-08 16:12:43 -0600

    RandomSelector function creation

--- internal/tools/random.go
+++ internal/tools/random.go
@@ -1,12 +1,19 @@
 package tools
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
-func RandomSelector(r []string) {
+func RandomSelector(r []string) int {
+	
+	min := 0
+	max := len(r)
 
+	rand.Seed(time.Now().UnixNano())
+
+	selector := rand.Intn(max - min)
+	
+	return selector
 
 }
