ccbc7002ce61d0cc3b15e27e03af8581dd4ba966
Nick Mancari <nickjmancari@gmail.com>
2023-03-20 16:06:15 -0500

    better test setup

--- cmd/placebo/cmd/file_test.go
+++ cmd/placebo/cmd/file_test.go
@@ -8,19 +8,22 @@ import (
 
 func TestFile(t *testing.T) {
 
-	os.Args = []string{"placebo", "csv", ""}
+	//os.Args = []string{"placebo", "csv", ""}
 
 	var tests = []struct {
 		description string
 		want        error
 		input       string
+		args        []string
 	}{
-		{"Empty String", nil, ""},
-		{"Invalid Input", errors.New("not a Valid Command"), "taco"},
-		{"Default 'csv' command", nil, "csv"},
+		{"Empty String", nil, "", []string{""}},
+		{"Invalid Input", errors.New("not a Valid Command"), "taco", []string{""}},
+		{"Default 'csv' command", nil, "csv", []string{"placebo", "csv", ""}},
+		{"Pass in Numbers", nil, "csv", []string{"placebo", "csv", "3"}},
 	}
 	for _, tc := range tests {
 		t.Run(tc.description, func(t *testing.T) {
+			os.Args = tc.args
 			got := File(tc.input)
 			if (got == nil) != (tc.want == nil) || (got != nil && got.Error() != tc.want.Error()) {
 				t.Errorf("File(%s)=%v, want %v", tc.input, got, tc.want)
