defc8f28a58a14ee68da5dd2ec5ad81faaadef03
Nick Mancari <nickjmancari@gmail.com>
2023-03-20 13:09:26 -0500

    test placeholder

---
+++ internal/tools/random_test.go
@@ -0,0 +1,26 @@
+package tools
+
+import (
+	"testing"
+)
+
+func TestRandomSelector(t *testing.T) {
+	var tests = []struct {
+		description string
+		want        int
+		input       []string
+	}{
+		{"List of Names", 0, []string{"John"}},
+		{"List of Places", 0, []string{"NEW YORK"}},
+		{"Numbers as Strings", 0, []string{"1"}},
+		{"Empty String", 0, []string{""}},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := RandomSelector(tc.input)
+			if got != tc.want {
+				t.Fatalf("RandomSelector(%s)=%v, want %v", tc.input, got, tc.want)
+			}
+		})
+	}
+}
