e37b5420e3f9ea17fa1fa51463fd0a9f6f777fc3
Nick Mancari <nickjmancari@gmail.com>
2023-03-20 19:06:21 -0500

    changing ints for test

--- internal/tools/random_test.go
+++ internal/tools/random_test.go
@@ -11,8 +11,8 @@ func TestRandomSelector(t *testing.T) {
 		want        int
 		input       []string
 	}{
-		{"List of Names", 0, []string{"John"}},
-		{"List of Places", 0, []string{"NEW YORK"}},
+		{"List of Names", 1, []string{"John", "Bill"}},
+		{"List of Places", 2, []string{"NEW YORK", "DENVER", "DALLAS"}},
 		{"Numbers as Strings", 0, []string{"1"}},
 		{"Empty String", 0, []string{""}},
 	}
@@ -20,7 +20,7 @@ func TestRandomSelector(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.description, func(t *testing.T) {
 			got := RandomSelector(tc.input)
-			if got != tc.want {
+			if got != tc.want && got >= tc.want {
 				t.Fatalf("RandomSelector(%s)=%v, want %v", tc.input, got, tc.want)
 			}
 		})
