66a0d447bdcf5c21e838bd72c338f890508e4ca7
Nick Mancari <nickjmancari@gmail.com>
2023-04-04 22:45:55 -0500

    phone construction tests

---
+++ pkg/random/phone_test.go
@@ -0,0 +1,50 @@
+package random
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPatient_PhoneNumber(t *testing.T) {
+
+	testPatient := &Patient{}
+
+	testPatientAddressAndPhone := testPatient.NewAddress()
+
+	var tests = []struct {
+		description string
+		expected    string
+	}{
+		{"Default Case", "1234567"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := testPatientAddressAndPhone.PhoneNumber()
+			if !strings.Contains(got.Phone, tc.expected) {
+				t.Fatalf("Phone()=%v should containt %v", got.Phone, tc.expected)
+			}
+		})
+	}
+}
+
+func TestAreaCode(t *testing.T) {
+	var tests = []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{"Default Case of City in the List", "DOVER", "302"},
+		{"Case Where City isn't in the List", "ST. PAUL", ""},
+		{"Case Where City is Bad Format", "Dover", ""},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := AreaCode(tc.input)
+			if got != tc.expected {
+				t.Fatalf("AreaCode(%v)=%v, expected %v and got %v", tc.input, got, tc.expected, got)
+			}
+		})
+	}
+}
