bb7ceb619a5de2665229370f1cbc4ddf6c292d87
nickmancari <nickjmancari@gmail.com>
2023-10-31 15:08:05 -0500

    hl7 send command placeholder test

--- cmd/placebo/cmd/send.go
+++ cmd/placebo/cmd/send.go
@@ -1,1 +1,49 @@
 package cmd
+
+import (	
+	"text/template"
+//	"os"
+	"bytes"
+	"fmt"
+	
+//	"placebo/internal/network"
+	"placebo/pkg/random"
+	"placebo/pkg/templates"
+)
+
+func SendHl7Message(f string) error {
+	
+	switch f {
+	case "":
+		return nil
+	case "hl7":
+		patient := random.NewPatient()
+		hl7 := Hl7Format(patient)
+
+		fmt.Println(hl7)
+
+		return nil
+
+	}
+
+	return nil
+
+}
+
+func Hl7Format(p *random.Patient) string {
+
+	temp := templates.SimpleHl7Info()
+
+	t, err := template.New("hl7").Parse(string(temp))
+	if err != nil {
+		panic(err)
+	}
+
+	var tpl bytes.Buffer
+	t.Execute(&tpl, p)
+
+	result := tpl.String()
+
+	return result
+
+}
--- cmd/placebo/main.go
+++ cmd/placebo/main.go
@@ -9,10 +9,17 @@ import (
 
 var randomFile = flag.String("file", "", "Creates a csv file with a fake patient at /tmp/.\n\t'csv' command creates a random csv file.\n\t\tadding numbers to the 'csv' command will produce multiple fake patients i.e. 'placebo --file csv 4'")
 
+var sendHl7 = flag.String("send", "", "")
+
 func main() {
 	flag.Parse()
 
 	err := cmd.File(*randomFile)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	err = cmd.SendHl7Message(*sendHl7)
 	if err != nil {
 		fmt.Println("ERROR: ", err)
 	}
