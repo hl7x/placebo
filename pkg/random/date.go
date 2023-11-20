5286219741e224e967270b75ebb8b8b88ae62a75
nickmancari <nickjmancari@gmail.com>
2023-11-20 09:39:13 -0600

    date formatter

--- pkg/random/date.go
+++ pkg/random/date.go
@@ -3,6 +3,7 @@ package random
 import (
 	"math/rand"
 	"fmt"
+	"strconv"
 )
 
 func Date() string {
@@ -48,3 +49,29 @@ func Year() int {
 
 	return year
 }
+
+func Hl7Formatter(date string) string {
+	
+	split := strings.Split(date, "-")
+
+	var digits []string
+
+	for _, number := range split {
+		parse, err := strconv.Atoi(number)
+		if err != nil {
+			panic(err)
+		}
+
+		if parse < 10 {
+			number = "0" + number
+		}
+
+		digits = append(digits, number)
+	}
+
+	format := fmt.Sprintf("%v%v%v", digits[2], digits[0], digits[1])
+
+	return format
+
+}
+
