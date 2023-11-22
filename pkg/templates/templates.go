fb8259269dddfea590d7c51a6234d4f49d9b2b76
nickmancari <nickjmancari@gmail.com>
2023-11-22 11:58:09 -0600

    event date templating

--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -25,9 +25,9 @@ func CSVPatientInfo() []byte {
 func SimpleHl7Info() []byte {
 
 	hl7Example := []byte(`
-MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|202310060800||ADT^A01|12345|P|2.3|
-EVN|A01|202310060800||
-PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12345|
+MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|{{.Hl7Info.HL7Event}}0800||ADT^A01|12345|P|2.3|
+EVN|A01|{{.Hl7Info.HL7Event}}0800||
+PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7Info.HL7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12346|
 PV1|1|IP|PUNIT^RM 123^P^PUNIT|ER|||15551234567^HOUSE^GREG|1255568135^BETA^ALPHA|30104384^SMITH^JOHN|HIM||||Phys/Clinic|||123456789^BETA^ALPHA|TESTX|{{.EncounterId}}|||||||||||||||||||||||||{{.Hl7Info.HL7Arrival}}|||
 `)
 
@@ -39,9 +39,9 @@ PV1|1|IP|PUNIT^RM 123^P^PUNIT|ER|||15551234567^HOUSE^GREG|1255568135^BETA^ALPHA|
 func DischargeHl7Info() []byte {
 
 	dischargeHl7 := []byte(`
-MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|202310060800||ADT^A03|12345|P|2.3|
-EVN|A03|202310060800||
-PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12345|
+MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|{{.Hl7Info.HL7Event}}0800||ADT^A03|12345|P|2.3|
+EVN|A03|{{.Hl7Info.HL7Event}}0800||
+PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7Info.HL7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12345|
 PV1|1|DC|PUNIT^RM 123^P^PUNIT|ER|||15551234567^HOUSE^GREG|1255568135^BETA^ALPHA|30104384^SMITH^JOHN|HIM||||Phys/Clinic|||123456789^BETA^ALPHA|TESTX|{{.EncounterId}}||||||||||||||||||||||||||{{.Hl7Info.HL7Discharge}}||
 
 `)
@@ -53,9 +53,9 @@ PV1|1|DC|PUNIT^RM 123^P^PUNIT|ER|||15551234567^HOUSE^GREG|1255568135^BETA^ALPHA|
 func ScheduledPatientInfo() []byte {
 
 	scheduleSiu := []byte(`
-MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|202310060800||SIU^S12|12345|P|2.3|
+MSH|^~\&|SENDAPP|PLACEBO|RECVAPP|LAB|{{.Hl7Info.HL7Event}}0800||SIU^S12|12345|P|2.3|
 SCH|12345|12345|OFFICE^123^PUNIT|20231010|0800|||60|M^Minutes|Regular Appointment||{{.AppointmentId}}|||Scheduled|
-PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12345|
+PID|1|56789|{{.MRN}}||{{.LastName}}^{{.FirstName}}||{{.Hl7Info.HL7DOB}}|M|||{{.PatientAddress.StructureNumber}} {{.PatientAddress.Street}}^^{{.PatientAddress.RegionInfo.City}}^{{.PatientAddress.RegionInfo.State}}^12345|
 RGS|1|A|
 `)
 
