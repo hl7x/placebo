67ea2fbd89bb0ab6ef9268379cc2c8c7dd176215
nickmancari <nickjmancari@gmail.com>
2023-11-28 16:16:53 -0600

    staging hl7 commands

--- cmd/placebo/cmd/send.go
+++ cmd/placebo/cmd/send.go
@@ -4,6 +4,7 @@ import (
 	"text/template"
 	"bytes"
 	"fmt"
+	"os"
 	
 	"placebo/internal/network"
 	"placebo/pkg/random"
@@ -16,17 +17,38 @@ func SendHl7Message(f string) error {
 	case "":
 		return nil
 	case "hl7":
-		patient := random.NewPatient()
-		hl7 := Hl7Format(patient)
+		
+		command := os.Args[3:]
 
-		err := DefaultSend(hl7)
-		if err != nil {
-			return err
+		if len(command) == 0 {
+			
+			patient := random.NewPatient()
+			template := templates.SimpleHl7Info()
+
+			hl7 := Hl7Format(patient, template)
+
+			err := DefaultSend(hl7)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("HL7 Sent:\n %v", hl7)
+
+			return nil
+
+		} else if command[0] == "post_admit" {
+			//post_admit hl7 event
+		} else if command[0] == "appointment" {
+			//appointment siu event
+		} else if command[0] == "pre_admit" {
+			// scheduled pre-admit event
+		} else if command[0] == "post_discharge" {
+			//post_discharge hl7 event
+			err := EventSelector(command[0])
+			if err != nil {
+				return err
+			}
 		}
-
-		fmt.Printf("HL7 Sent:\n %v", hl7)
-
-		return nil
 
 	}
 
@@ -34,9 +56,9 @@ func SendHl7Message(f string) error {
 
 }
 
-func Hl7Format(p *random.Patient) string {
+func Hl7Format(p *random.Patient, temp []byte) string {
 
-	temp := templates.SimpleHl7Info()
+//	temp := templates.SimpleHl7Info()
 
 	t, err := template.New("hl7").Parse(string(temp))
 	if err != nil {
@@ -63,6 +85,39 @@ func DefaultSend(templatePatient string) error {
 	err := network.SendClient(address, port, templatePatient)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func EventSelector(s string) error {
+
+	switch s {
+	case "":
+		return nil
+	case "post_discharge":
+		patient := random.NewPatient()
+		admitTemp := templates.SimpleHl7Info()
+
+		admitHl7 := Hl7Format(patient, admitTemp)
+		err := DefaultSend(admitHl7)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("hl7 sent: \n", admitHl7)
+		fmt.Println("\n")
+
+		dischargeTemp := templates.DischargeHl7Info()
+
+		dischargeHl7 := Hl7Format(patient, dischargeTemp)
+		
+		err = DefaultSend(dischargeHl7)
+		if err != nil {
+			return nil
+		}
+
+		fmt.Println("hl7 sent: \n", dischargeHl7)
 	}
 
 	return nil
