1bf2ba5d48a111d83971c69aa2beaab1e38675dc
nickmancari <nickjmancari@gmail.com>
2024-03-14 16:12:47 -0500

    network client unit tests

--- internal/network/client_test.go
+++ internal/network/client_test.go
@@ -1,1 +1,42 @@
 package network
+
+import (
+	"testing"
+	"net"
+	"time"
+
+)
+
+func TestRequestHandler(t *testing.T) {
+	
+	duration := 2 * time.Second
+	timer := time.After(duration)
+
+	var tests = []struct {
+		description	string
+	}{
+		{"Should Accept Connection"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			go func() {
+
+				ln, err := net.Listen("tcp", ":9702")
+				if err != nil {
+					t.Fatalf("Connection Not starting...")
+				}
+
+				conn, _ := ln.Accept()
+				RequestHandler(conn)
+			}()
+
+			select {
+			case <- timer:
+				return
+			default:
+				return
+			}
+		})
+	}
+}
