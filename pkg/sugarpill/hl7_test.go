e60cb2806f569faf234872e60f9c15516df6d440
nickmancari <nickjmancari@gmail.com>
2024-05-16 15:36:53 -0500

    unit test start

--- pkg/sugarpill/hl7_test.go
+++ pkg/sugarpill/hl7_test.go
@@ -1,1 +1,45 @@
 package sugarpill
+
+import (
+	"testing"
+
+	"placebo/pkg/random"
+)
+
+var sugarpillPatient *random.Patient
+
+func SugarpillPatient() *random.Patient {
+
+	patient := random.NewPatient()
+
+	return patient
+
+}
+
+func TestMain(m *testing.M) {
+	sugarpillPatient = SugarpillPatient()
+	m.Run()
+
+}
+
+func TestNewPV1Segment(t *testing.T) {
+	
+	var tests = []struct{
+		description	string
+		input		*random.Patient
+		expected	interface{}
+	}{
+		{"PV1 Visit Number Should Be the Same From Patient", sugarpillPatient, sugarpillPatient.EncounterId},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T ) {
+			got := NewPV1Segment(tc.input)
+
+			if got.VisitNumber != tc.expected {
+				t.Fatalf("got %v, expected %v", got.VisitNumber, tc.expected)
+			}
+		})
+	}
+}
+
