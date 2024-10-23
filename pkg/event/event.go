8361bd0adbb4ed25b4d364719be4aba0ee35c721
nickmancari <nickjmancari@gmail.com>
2024-10-23 17:01:23 -0500

    starting place

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -10,6 +10,12 @@ import (
 
 /* Build HL7 message based on certain medical event for various scenarios */
 
+type Event struct {
+	MessageCode 	string
+	EventCode	string
+	Patient		*random.Patient
+}
+
 var MessageTypeCode = map[string]string{
 	"AdmitDischargeTransfer": "ADT",
 	"SchedulingInformationUnsolicited": "SIU",
@@ -24,6 +30,65 @@ var EventTypeCode = map[string]string{
 	"referral": "I12",
 }
 
+/*
+func Build(eventScenario []string) []string {
+	
+	var messages []string
+	
+	patient := random.NewPatient()
+
+	for _, message := range eventScenario {
+		// process event command to build event
+	}
+
+	return messages
+}
+*/
+
+func NewEvent(p *random.Patient, eventCommand string, messageCommand string) *Event {
+
+	event := Event{}
+
+	eventPatient := event.EventPatient(p)
+
+	message := eventPatient.MessageCode(messageCommand)
+
+	messageEventType := message.EventCode(eventCommand)
+
+	return messageEventType
+}
+
+// MessageCode Setter
+func (e *Event) MessageCode(code string) *Event {
+
+	messageCode := MessageTypeCode[code]
+
+	e.MessageCode = messageCode
+
+	return e
+}
+
+// EventCode Setter
+func (e *Event) EventCode(code string) *Event {
+
+	eventCode := EventTypeCode[code]
+
+	e.EventCode = eventCode
+
+	return e
+
+}
+
+// Patient Setter
+func (e *Event) EventPatient(p *random.Patient) *Event {
+
+	e.Patient = p
+
+	return e
+
+}
+
+// Obsolete: for template construction
 func Builder(scenario []string) []string {
 
 	var messages []string
@@ -43,6 +108,7 @@ func Builder(scenario []string) []string {
 
 }
 
+// Obsolete: for template construction
 func TemplateFinder(s string) []byte {
 
 	switch s {
@@ -62,6 +128,7 @@ func TemplateFinder(s string) []byte {
 
 }
 
+// Obsolete: for template construction
 func TemplateMapper(p *random.Patient, temp []byte) string {
 
 	t, err := template.New("hl7").Parse(string(temp))
