8606502444256e6a36df956f712bf96256c17f2a
Nick Mancari <nickjmancari@gmail.com>
2026-05-21 12:30:37 -0500

    new segs for important ones

--- pkg/sugarpill/hl7.go
+++ pkg/sugarpill/hl7.go
@@ -12,16 +12,21 @@ type HL7Message struct {
 	MSH *MSH `json:"MSH"`
 	EVN *EVN `json:"EVN"`
 	PID *PID `json:"PID"`
+	PD1 *PD1 `json:"PD1"`
 	ROL *ROL `json:"ROL"`
 	DB1 *DB1 `json:"DB1"`
 	ARV *ARV `json:"ARV"`
+	NK1 *NK1 `json:"NK1"`
 	PV1 *PV1 `json:"PV1"`
 	PV2 *PV2 `json:"PV2"`
-	DG1 *DG1 `json:"DG1"`
-	OBX *OBX `json:"OBX"`
-	NK1 *NK1 `json:"NK1"`
+	GT1 *GT1 `json:"GT1"`
+	IN1 *IN1 `json:"IN1"`
 	AL1 *AL1 `json:"AL1"`
+	DG1 *DG1 `json:"DG1"`
+	ORC *ORC `json:"ORC"`
 	OBR *OBR `json:"OBR"`
+	NTE *NTE `json:"NTE"`
+	OBX *OBX `json:"OBX"`
 }
 
 func NewHL7Message(p *random.Patient) *HL7Message {
@@ -30,16 +35,21 @@ func NewHL7Message(p *random.Patient) *HL7Message {
 		MSH: NewMSHSegment(p),
 		EVN: NewEVNSegment(p),
 		PID: NewPIDSegment(p),
+		PD1: NewPD1Segment(p),
 		ROL: NewROLSegment(p),
 		DB1: NewDB1Segment(p),
 		ARV: NewARVSegment(p),
+		NK1: NewNK1Segment(p),
 		PV1: NewPV1Segment(p),
 		PV2: NewPV2Segment(p),
-		DG1: NewDG1Segment(p),
-		OBX: NewOBXSegment(p),
-		NK1: NewNK1Segment(p),
+		GT1: NewGT1Segment(p),
+		IN1: NewIN1Segment(p),
 		AL1: NewAL1Segment(p),
+		DG1: NewDG1Segment(p),
+		ORC: NewORCSegment(p),
 		OBR: NewOBRSegment(p),
+		NTE: NewNTESegment(p),
+		OBX: NewOBXSegment(p),
 	}
 
 	return message
@@ -115,26 +125,36 @@ func SegmentFinder(s string) interface{} {
 		return &EVN{}
 	case "PID":
 		return &PID{}
+	case "PD1":
+		return &PD1{}
 	case "ROL":
 		return &ROL{}
 	case "DB1":
 		return &DB1{}
 	case "ARV":
 		return &ARV{}
+	case "NK1":
+		return &NK1{}
 	case "PV1":
 		return &PV1{}
 	case "PV2":
 		return &PV2{}
-	case "DG1":
-		return &DG1{}
-	case "OBX":
-		return &OBX{}
-	case "NK1":
-		return &NK1{}
+	case "GT1":
+		return &GT1{}
+	case "IN1":
+		return &IN1{}
 	case "AL1":
 		return &AL1{}
+	case "DG1":
+		return &DG1{}
+	case "ORC":
+		return &ORC{}
 	case "OBR":
 		return &OBR{}
+	case "NTE":
+		return &NTE{}
+	case "OBX":
+		return &OBX{}
 	default:
 		return nil
 	}
@@ -149,26 +169,36 @@ func (msg *HL7Message) SegmentAssigner(s interface{}) *HL7Message {
 		msg.EVN = v
 	case *PID:
 		msg.PID = v
+	case *PD1:
+		msg.PD1 = v
 	case *ROL:
 		msg.ROL = v
 	case *DB1:
 		msg.DB1 = v
 	case *ARV:
 		msg.ARV = v
+	case *NK1:
+		msg.NK1 = v
 	case *PV1:
 		msg.PV1 = v
 	case *PV2:
 		msg.PV2 = v
-	case *DG1:
-		msg.DG1 = v
-	case *OBX:
-		msg.OBX = v
-	case *NK1:
-		msg.NK1 = v
+	case *GT1:
+		msg.GT1 = v
+	case *IN1:
+		msg.IN1 = v
 	case *AL1:
 		msg.AL1 = v
+	case *DG1:
+		msg.DG1 = v
+	case *ORC:
+		msg.ORC = v
 	case *OBR:
 		msg.OBR = v
+	case *NTE:
+		msg.NTE = v
+	case *OBX:
+		msg.OBX = v
 	default:
 		return nil
 	}
@@ -181,16 +211,21 @@ func MessageBuilder(msg *HL7Message) string {
 		message.CreateHL7(msg.MSH, "MSH"),
 		message.CreateHL7(msg.EVN, "EVN"),
 		message.CreateHL7(msg.PID, "PID"),
+		message.CreateHL7(msg.PD1, "PD1"),
 		message.CreateHL7(msg.ROL, "ROL"),
 		message.CreateHL7(msg.DB1, "DB1"),
 		message.CreateHL7(msg.ARV, "ARV"),
+		message.CreateHL7(msg.NK1, "NK1"),
 		message.CreateHL7(msg.PV1, "PV1"),
 		message.CreateHL7(msg.PV2, "PV2"),
-		message.CreateHL7(msg.DG1, "DG1"),
-		message.CreateHL7(msg.OBX, "OBX"),
-		message.CreateHL7(msg.NK1, "NK1"),
+		message.CreateHL7(msg.GT1, "GT1"),
+		message.CreateHL7(msg.IN1, "IN1"),
 		message.CreateHL7(msg.AL1, "AL1"),
+		message.CreateHL7(msg.DG1, "DG1"),
+		message.CreateHL7(msg.ORC, "ORC"),
 		message.CreateHL7(msg.OBR, "OBR"),
+		message.CreateHL7(msg.NTE, "NTE"),
+		message.CreateHL7(msg.OBX, "OBX"),
 	}
 
 	return strings.Join(segments, "\n")
---
+++ pkg/sugarpill/pd1.go
@@ -0,0 +1,39 @@
+package sugarpill
+
+import "placebo/pkg/random"
+
+type PD1 struct {
+	LivingDependency                   string       `json:"LivingDependency"`                   // PD1-1
+	LivingArrangement                  string       `json:"LivingArrangement"`                  // PD1-2
+	PatientPrimaryFacility             *XON         `json:"PatientPrimaryFacility"`             // PD1-3
+	StudentIndicator                   string       `json:"StudentIndicator"`                   // PD1-5
+	Handicap                           string       `json:"Handicap"`                           // PD1-6
+	LivingWillCode                     string       `json:"LivingWillCode"`                     // PD1-7
+	OrganDonorCode                     string       `json:"OrganDonorCode"`                     // PD1-8
+	SeparateBillFlag                   string       `json:"SeparateBillFlag"`                   // PD1-9
+	DuplicatePatient                   *CX          `json:"DuplicatePatient"`                   // PD1-10
+	PublicityCode                      *ServiceCode `json:"PublicityCode"`                      // PD1-11
+	ProtectionIndicator                string       `json:"ProtectionIndicator"`                // PD1-12
+	ProtectionIndicatorEffectiveDate   string       `json:"ProtectionIndicatorEffectiveDate"`   // PD1-13
+	PlaceOfWorship                     *XON         `json:"PlaceOfWorship"`                     // PD1-14
+	AdvanceDirectiveCode               *ServiceCode `json:"AdvanceDirectiveCode"`               // PD1-15
+	ImmunizationRegistryStatus         string       `json:"ImmunizationRegistryStatus"`         // PD1-16
+	ImmunizationRegistryStatusEffectiveDate string  `json:"ImmunizationRegistryStatusEffectiveDate"` // PD1-17
+	PublicityCodeEffectiveDate         string       `json:"PublicityCodeEffectiveDate"`         // PD1-18
+	MilitaryBranch                     string       `json:"MilitaryBranch"`                     // PD1-19
+	MilitaryRankGrade                  string       `json:"MilitaryRankGrade"`                  // PD1-20
+	MilitaryStatus                     string       `json:"MilitaryStatus"`                     // PD1-21
+}
+
+func NewPD1Segment(p *random.Patient) *PD1 {
+
+	pd1 := &PD1{
+		PatientPrimaryFacility: &XON{},
+		DuplicatePatient:       &CX{},
+		PublicityCode:          &ServiceCode{},
+		PlaceOfWorship:         &XON{},
+		AdvanceDirectiveCode:   &ServiceCode{},
+	}
+
+	return pd1
+}
