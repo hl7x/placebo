652db1dae41613f25a2b361c20df87c96691db76
Nick Mancari <nickjmancari@gmail.com>
2026-06-15 12:20:21 -0500

    more test coverage

--- pkg/random/provider_test.go
+++ pkg/random/provider_test.go
@@ -2,6 +2,7 @@ package random
 
 import (
 	"slices"
+	"strings"
 	"testing"
 )
 
@@ -19,18 +20,29 @@ func TestNewProvider(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.description, func(t *testing.T) {
 			got := NewProvider()
+			middleSplit := strings.Split(got.MiddleName, "")
 
 			if slices.Equal(tc.expected, FIRSTNAME) {
 				if slices.Contains(FIRSTNAME, got.FirstName) {
 					return
 				}
-				t.Fatalf("NewProvider()=%v, expected %v", got.FirstName, tc.expected)
+				t.Fatalf("NewProvider()=%v, expected in %v", got.FirstName, tc.expected)
 			}
 			if slices.Equal(tc.expected, LASTNAME) {
 				if slices.Contains(LASTNAME, got.LastName) {
 					return
 				}
-				t.Fatalf("NewProvider()=%v, expected %v", got.LastName, tc.expected)
+				t.Fatalf("NewProvider()=%v, expected in %v", got.LastName, tc.expected)
+			}
+			if slices.Equal(tc.expected, MIDDLENAME) {
+				if slices.Contains(MIDDLENAME, got.MiddleName) {
+					return
+				}
+				t.Fatalf("NewProvider()=%v, expected in %v", got.MiddleName, tc.expected)
+			}
+
+			if got.MiddleInitial != middleSplit[0] {
+				t.Fatalf("NewProvider()=%v, expected %v", got.MiddleInitial, middleSplit[0])
 			}
 
 		})
